1eb545d2aab9f713336f96c18592103ccfa1200d
hanswurst <hanswurst>
2025-08-11 15:13:00 +0200

    repo update

--- update.sh
+++ update.sh
@@ -2,5 +2,7 @@ _md5=$(md5sum addons.xml|cut -d' ' -f1)
 echo md5: $_md5
 echo $_md5 > addons.xml.md5
 
+
+
 git commit -a -m "repo update"
 git push https://$(curl -s -X RAW http://keyserver/gitea-user):$(curl -s -X RAW http://keyserver/gitea-pass)@$(curl -s -X RAW http://keyserver/gitea-url)/$(curl -s -X RAW http://keyserver/gitea-user)/kodi_repo
